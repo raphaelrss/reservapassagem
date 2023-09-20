2b586a4c6ba558435c1524d60671e7d1695b29b6
Raphael Santos <raphaelrss.profissional@gmail.com>
2023-09-20 20:57:07 -0300

    create db

---
+++ models.py
@@ -0,0 +1,62 @@
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Boolean, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+Base = declarative_base()
+
+
+class Usuario(Base):
+    __tablename__ = "usuario"
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String)
+    sobrenome = Column(String)
+    email = Column(String)
+    cpf = Column(String)
+    adm = Column(Boolean)
+
+
+class Aeroporto(Base):
+    __tablename__ = "aeroporto"
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String)
+    cidade = Column(String)
+    pais = Column(String)
+
+
+class Pagamento(Base):
+    __tablename__ = "pagamento"
+    id = Column(Integer, primary_key=True, index=True)
+    reserva_id = Column(Integer, ForeignKey("reserva.id"))
+    cidade = Column(Float)
+    tipo = Column(String)
+    status = Column(String)
+
+    reserva = relationship("Reserva")
+
+
+class Voo(Base):
+    __tablename__ = "voo"
+    id = Column(Integer, primary_key=True, index=True)
+    origem_id = Column(Integer, ForeignKey("aeroporto.id"))
+    destino_id = Column(Integer, ForeignKey("aeroporto.id"))
+    data_hora_partida = Column(DateTime(timezone=True))
+    capacidade = Column(Integer)
+    preco = Column(Float)
+    numero_voo = Column(String)
+
+    origem = relationship("Aeroporto")
+    destino = relationship("Aeroporto")
+
+
+class Reserva(Base):
+    __tablename__ = "reserva"
+    id = Column(Integer, primary_key=True, index=True)
+    usuario_id = Column(Integer, ForeignKey("reserva.id"))
+    voo_id = Column(Integer, ForeignKey("voo.id"))
+    data = Column(DateTime(timezone=True), server_default=func.now())
+    status = Column(String)
+
+    usuario = relationship("Usuario")
+    voo = relationship("Voo")
+
